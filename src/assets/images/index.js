c23cfd55a8e7312c47e8fd69b72de63f14ec3bda
Kimbang89 <trankimbang0809@gmail.com>
2023-02-10 14:45:20 +0700

    fix lỗi nhiều file index

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,1 @@
-const images = {
-    //images.logo trả về object có key là default chứa link svg
-    logo: require('~/assets/images/logo.svg').default,
-    noImage: require('~/assets/images/no-avatar.png'),
-};
-export default images;
+export { default } from './images';
